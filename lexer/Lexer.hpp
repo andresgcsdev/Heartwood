5b1ba1a5333490553c548bb00091c4eb2bd30a57
andre <andredesenvolvimentosis@gmail.com>
2026-05-21 04:41:32 -0300

    Fix comment

--- lexer/Lexer.hpp
+++ lexer/Lexer.hpp
@@ -6,5 +6,6 @@
 namespace Lexer
 {
     // Converts .hw file at filepath to a Token vector to be consumed by Parser.
+    // May raise an error at undefined tokens.
     std::vector<Token> tokenize(const std::string& filepath);
 }

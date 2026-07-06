b3ace6da86c86c1bc66a10888472a710fe128ec5
André <andredesenvolvimentosis@gmail.com>
2026-07-05 22:25:28 -0300

    Added functions to generalize some TokenTypes into groups

--- lexer/Token.hpp
+++ lexer/Token.hpp
@@ -28,3 +28,26 @@ struct Token
     std::string value; // For identifiers or literals.
     int line; // For better error messages.
 };
+
+// Basic generalization functions:
+
+inline bool isLiteral(const TokenType &t)
+{
+    return t == TokenType::LITERAL_INT || t == TokenType::LITERAL_FLOAT || t == TokenType::LITERAL_STR ||
+           t == TokenType::LITERAL_BOOL;
+}
+
+inline bool isOperator(const TokenType &t)
+{
+    return t == TokenType::PLUS || t == TokenType::MINUS || t == TokenType::MULTIPLY ||
+           t == TokenType::DIVIDE || t == TokenType::MODULO || t == TokenType::EQUALS ||
+           t == TokenType::NOT_EQUALS || t == TokenType::LESS || t == TokenType::LESS_EQUALS ||
+           t == TokenType::GREATER || t == TokenType::GREATER_EQUALS || t == TokenType::OR ||
+           t == TokenType::AND || t == TokenType::XOR || t == TokenType::NOT;
+}
+
+inline bool isVarType(const TokenType &t)
+{
+    return t == TokenType::TYPE_INT || t == TokenType::TYPE_FLOAT || t == TokenType::TYPE_STR ||
+           t == TokenType::TYPE_BOOL || t == TokenType::ARRAY;
+}

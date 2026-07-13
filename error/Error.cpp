a40467d656795d5d296994f4274d9cb3f95cc8c5
André <andredesenvolvimentosis@gmail.com>
2026-07-13 01:45:52 -0300

    Added tips parameter for better error messages

--- error/Error.cpp
+++ error/Error.cpp
@@ -1,14 +1,23 @@
 #include "Error.hpp"
 #include <iostream>
 
-void Error::raise(const Phase phase, const std::string& message, const int line) {
+void Error::raise(const Phase phase, const std::string &message, const int line, const std::string &tips)
+{
     std::string phaseStr;
-    switch(phase) {
-        case Phase::Lexer:    phaseStr = "Lexer";    break;
-        case Phase::Parser:   phaseStr = "Parser";   break;
-        case Phase::Semantic: phaseStr = "Semantic"; break;
-        case Phase::Runtime:  phaseStr = "Runtime";  break;
+    switch (phase)
+    {
+        case Phase::Lexer: phaseStr = "Lexer";
+            break;
+        case Phase::Parser: phaseStr = "Parser";
+            break;
+        case Phase::Semantic: phaseStr = "Semantic";
+            break;
+        case Phase::Runtime: phaseStr = "Runtime";
+            break;
     }
-    std::cerr << "[" << phaseStr << " Error] on line " << line << ": " << message << "\n";
+    std::cerr << "[" << phaseStr << " Error] on line " << line << ": " << message << std::endl;
+    if (!tips.empty())
+        std::cerr << tips << std::endl;
+
     std::exit(1);
 }
--- error/Error.hpp
+++ error/Error.hpp
@@ -3,17 +3,16 @@
 
 // Simple code Block for Error Raising.
 namespace Error
-{
-    // Determine which phase of the interpreter the error has occurred.
-    enum class Phase
     {
-        Lexer,
-        Parser,
-        Semantic,
-        Runtime
-    };
+        // Determine which phase of the interpreter the error has occurred.
+        enum class Phase
+        {
+            Lexer,
+            Parser,
+            Semantic,
+            Runtime
+        };
 
-    // Prints out the error and stops execution.
-    void raise(Phase phase, const std::string& message, int line);
-}
-
+        // Prints out the error and stops execution.
+        [[noreturn]] void raise(Phase phase, const std::string &message, int line, const std::string &tips = "");
+    }

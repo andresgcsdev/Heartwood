c914c769f087f6d0bc4f9002b0a577ef779f13cd
andre <andredesenvolvimentosis@gmail.com>
2026-07-19 00:19:52 -0300

    Set optional attributes as std::option instead of dangling nullptr

--- src/parser/AST.hpp
+++ src/parser/AST.hpp
@@ -7,212 +7,212 @@
 #include "../lexer/Token.hpp"
 
 namespace AST
+{
+    struct Node;
+
+    // Leave nodes - actual data
+
+    struct Literal
     {
-        struct Node;
+        Token value;
+    };
 
-        // Leave nodes - actual data
+    struct Identifier
+    {
+        Token name;
+    };
 
-        struct Literal
-        {
-            Token value;
-        };
+    // Data declaration
 
-        struct Identifier
-        {
-            Token name;
-        };
+    struct TypeNode; // Forward declaration
 
-        // Data declaration
+    struct PrimitiveType
+    {
+        Token name; // int, string, etc.
+    };
 
-        struct TypeNode; // Forward declaration
+    struct ArrayType
+    {
+        std::unique_ptr<TypeNode> element_type; // Primitive or ArrayType
+        std::unique_ptr<Node> size; // Expression, literal or identifier
+    };
 
-        struct PrimitiveType
-        {
-            Token name; // int, string, etc.
-        };
+    struct StructType
+    {
+        Token name;
+    };
 
-        struct ArrayType
-        {
-            std::unique_ptr<TypeNode> element_type; // Primitive or ArrayType
-            std::unique_ptr<Node> size; // Expression, literal or identifier
-        };
+    using TypeData = std::variant<PrimitiveType, ArrayType, StructType>;
 
-        struct StructType
-        {
-            Token name;
-        };
+    struct TypeNode
+    {
+        TypeData data;
+    };
 
-        using TypeData = std::variant<PrimitiveType, ArrayType, StructType>;
+    struct Parameter
+    {
+        Token name; // Identifier
+        TypeNode type;
+        bool is_reference = false;
+        bool is_mutable = false;
+        std::unique_ptr<Node> initializer; // Expression, literal or identifier
+    };
 
-        struct TypeNode
-        {
-            TypeData data;
-        };
+    struct VarDecl
+    {
+        Token name; // identifier
+        TypeNode type;
+        bool is_mutable = false;
+        std::optional<std::unique_ptr<Node> > initializer;
+        // Expression, Literal or Identifier - can be nullopt (default initializer)
+    };
 
-        struct Parameter
-        {
-            Token name; // Identifier
-            TypeNode type;
-            bool is_reference = false;
-            bool is_mutable = false;
-            std::unique_ptr<Node> initializer; // Expression, literal or identifier
-        };
+    struct StructDecl
+    {
+        Token name; // Identifier
+        std::vector<VarDecl> fields;
+    };
 
-        struct VarDecl
-        {
-            Token name; // identifier
-            TypeNode type;
-            bool is_mutable = false;
-            std::unique_ptr<Node> initializer;
-            // Expression, Literal or Identifier - can be nullptr (default initializer)
-        };
+    struct EnumDecl
+    {
+        Token name; // Identifier
+        std::vector<Token> members; // multiple Identifiers
+    };
 
-        struct StructDecl
-        {
-            Token name; // Identifier
-            std::vector<VarDecl> fields;
-        };
+    // Basic expressions - operations on the data
 
-        struct EnumDecl
-        {
-            Token name; // Identifier
-            std::vector<Token> members; // multiple Identifiers
-        };
+    struct UnaryExpr
+    {
+        Token op;
+        std::unique_ptr<Node> operand; // Expression, literal or identifier
+    };
 
-        // Basic expressions - operations on the data
+    struct BinaryExpr
+    {
+        Token op;
+        std::unique_ptr<Node> left; // Expression, literal or identifier
+        std::unique_ptr<Node> right; // Expression, literal or identifier
+    };
 
-        struct UnaryExpr
-        {
-            Token op;
-            std::unique_ptr<Node> operand; // Expression, literal or identifier
-        };
+    // Complex literals - literals that don't map directly into pure data
 
-        struct BinaryExpr
-        {
-            Token op;
-            std::unique_ptr<Node> left; // Expression, literal or identifier
-            std::unique_ptr<Node> right; // Expression, literal or identifier
-        };
+    struct StructLiteral
+    {
+        Token name; // mapping to a struct declaration
+        std::vector<std::pair<Token, std::unique_ptr<Node> > > members;
+    };
 
-        // Complex literals - literals that don't map directly into pure data
+    struct ArrayExpr
+    {
+        std::vector<std::unique_ptr<Node> > elements; // Expression, literal or identifier
+    };
 
-        struct StructLiteral
-        {
-            Token name; // mapping to a struct declaration
-            std::vector<std::pair<Token, std::unique_ptr<Node> > > members;
-        };
+    struct FnCall
+    {
+        Token callee; // an Identifier
+        std::vector<std::unique_ptr<Node> > arguments; // Expressions, literals or identifiers
+    };
 
-        struct ArrayExpr
-        {
-            std::vector<std::unique_ptr<Node> > elements; // Expression, literal or identifier
-        };
+    struct MemberAccess
+    {
+        std::unique_ptr<Node> object; // Grows from right to left
+        Token member;
+    };
 
-        struct FnCall
-        {
-            Token callee; // an Identifier
-            std::vector<std::unique_ptr<Node> > arguments; // Expressions, literals or identifiers
-        };
+    struct ArrayAccess
+    {
+        std::unique_ptr<Node> array;
+        std::unique_ptr<Node> index;
+    };
 
-        struct MemberAccess
-        {
-            std::unique_ptr<Node> object; // Grows from right to left
-            Token member;
-        };
+    struct EnumCall
+    {
+        Token callee; // an Identifier
+        Token member; // an Identifier
+    };
 
-        struct ArrayAccess
-        {
-            std::unique_ptr<Node> array;
-            std::unique_ptr<Node> index;
-        };
+    // Special operation
 
-        struct EnumCall
-        {
-            Token callee; // an Identifier
-            Token member; // an Identifier
-        };
+    struct Assign
+    {
+        std::unique_ptr<Node> target; // Can be Identifier, MemberAccess or ArrayAccess
+        std::unique_ptr<Node> value; // Literal, Identifier, Expression
+    };
 
-        // Special operation
+    // Scope - container of data and operations
 
-        struct Assign
-        {
-            std::unique_ptr<Node> target; // Can be Identifier, MemberAccess or ArrayAccess
-            std::unique_ptr<Node> value; // Literal, Identifier, Expression
-        };
+    struct Scope
+    {
+        std::vector<std::unique_ptr<Node> > statements;
+    };
 
-        // Scope - container of data and operations
+    // Code blocks - expressions that own scopes
 
-        struct Scope
-        {
-            std::vector<std::unique_ptr<Node> > statements;
-        };
+    struct If
+    {
+        std::unique_ptr<Node> condition; // an Expression node
+        std::unique_ptr<Node> thenBranch; // a ScopeNode or a single line of instructions
+        std::optional<std::unique_ptr<Node> > elseBranch; // a ScopeNode, a single line of instructions or nullopt
+    };
 
-        // Code blocks - expressions that own scopes
-
-        struct If
-        {
-            std::unique_ptr<Node> condition; // an Expression node
-            std::unique_ptr<Node> thenBranch; // a ScopeNode or a single line of instructions
-            std::unique_ptr<Node> elseBranch; // a ScopeNode, a single line of instructions or nullptr
-        };
-
-        struct While
-        {
-            std::unique_ptr<Node> condition; // an Expression node
-            std::unique_ptr<Node> body; // a ScopeNode or a single line of instructions
-        };
+    struct While
+    {
+        std::unique_ptr<Node> condition; // an Expression node
+        std::unique_ptr<Node> body; // a ScopeNode or a single line of instructions
+    };
 
 
-        struct For
-        {
-            std::optional<VarDecl> localCountVariable;
-            std::unique_ptr<Node> initialAssign; // nullptr if there is a localCountVariable
-            std::unique_ptr<Node> condition; // an Expression node
-            std::unique_ptr<Node> operation; // an Expression node
-            std::unique_ptr<Node> body; // a ScopeNode or a single line of instructions
-        };
+    struct For
+    {
+        std::optional<VarDecl> localCountVariable;
+        std::optional<std::unique_ptr<Node> > initialAssign; // nullopt if there is a localCountVariable
+        std::unique_ptr<Node> condition; // an Expression node
+        std::unique_ptr<Node> operation; // an Expression node
+        std::unique_ptr<Node> body; // a ScopeNode or a single line of instructions
+    };
 
-        struct FunctionDef
-        {
-            Token name; // identifier
-            std::optional<TypeNode> type;
-            std::vector<Parameter> parameters; // identifier + type + value
-            Scope body; // a ScopeNode
-        };
+    struct FunctionDef
+    {
+        Token name; // identifier
+        std::optional<TypeNode> type;
+        std::vector<Parameter> parameters; // identifier + type + value
+        Scope body; // a ScopeNode
+    };
 
-        struct GlobalBlock
-        {
-            std::vector<VarDecl> declarations;
-        };
+    struct GlobalBlock
+    {
+        std::vector<VarDecl> declarations;
+    };
 
-        // Exit
+    // Exit
 
-        struct Return
-        {
-            std::unique_ptr<Node> value; // Expression, Literal or Identifier - can be nullptr
-        };
+    struct Return
+    {
+        std::optional<std::unique_ptr<Node> > value; // Expression, Literal or Identifier - can be nullopt
+    };
 
-        struct Break
-        {
-            Token where;
-        };
+    struct Break
+    {
+        Token where;
+    };
 
-        struct Continue
-        {
-            Token where;
-        };
+    struct Continue
+    {
+        Token where;
+    };
 
-        // Polymorphism
+    // Union
 
-        using NodeData =
-        std::variant<
-            Literal, Identifier, UnaryExpr, FnCall, ArrayAccess,
-            BinaryExpr, Scope, If, Assign, ArrayExpr, GlobalBlock,
-            While, For, FunctionDef, MemberAccess, StructLiteral,
-            VarDecl, StructDecl, EnumDecl, Return, EnumCall, Break, Continue
-        >;
+    using NodeData =
+    std::variant<
+        Literal, Identifier, UnaryExpr, FnCall, ArrayAccess,
+        BinaryExpr, Scope, If, Assign, ArrayExpr, GlobalBlock,
+        While, For, FunctionDef, MemberAccess, StructLiteral,
+        VarDecl, StructDecl, EnumDecl, Return, EnumCall, Break, Continue
+    >;
 
-        struct Node
-        {
-            NodeData data;
-        };
-    }
+    struct Node
+    {
+        NodeData data;
+    };
+}

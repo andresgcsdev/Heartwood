8fe48aea81431d5dabdad946c1103137eea11348
andre <andredesenvolvimentosis@gmail.com>
2026-07-29 00:45:35 -0300

    Error module rework.

--- src/error/Error.cpp
+++ src/error/Error.cpp
@@ -1,23 +1,59 @@
 #include "Error.hpp"
 #include <iostream>
 
-void Error::raise(const Phase phase, const std::string &message, const int line, const std::string &tips)
+
+void ErrorList::add(const ErrorPhase &phase, const std::string &message, const Token &token, const std::string &tips)
 {
-    std::string phaseStr;
+    auto e = ErrorNode(phase, token, message, tips);
+    if (e.token.type == TokenType::EoF)
+        e.tips = "File ended without completing this previous instruction. Here's what's probably missing or wrong: " +
+                 e.tips;
+    else
+        e.tips = "What might be wrong: " + e.tips;
+
+    errors.push_back(e);
+}
+
+void ErrorList::raise(const ErrorPhase &phase)
+{
+    std::string masterPhaseStr;
     switch (phase)
     {
-        case Phase::Lexer: phaseStr = "Lexer";
+        case ErrorPhase::Lexer: masterPhaseStr = "Lexer";
             break;
-        case Phase::Parser: phaseStr = "Parser";
+        case ErrorPhase::Parser: masterPhaseStr = "Parser";
             break;
-        case Phase::Semantic: phaseStr = "Semantic";
+        case ErrorPhase::Semantic: masterPhaseStr = "Semantic analysis";
             break;
-        case Phase::Runtime: phaseStr = "Runtime";
+        case ErrorPhase::Runtime: masterPhaseStr = "Runtime";
             break;
     }
-    std::cerr << "[" << phaseStr << " Error] on line " << line << ": " << message << std::endl;
-    if (!tips.empty())
-        std::cerr << "What might be wrong: " << tips << std::endl;
+
+    if (errors.empty())
+    {
+        std::cout << "<<No errors found at "+ masterPhaseStr +" execution.>>" << std::endl;
+        return;
+    }
+
+    for (const auto &[nodePhase, token, message, tips]: errors)
+    {
+        std::string phaseStr;
+        switch (nodePhase)
+        {
+            case ErrorPhase::Lexer: phaseStr = "Lexer";
+                break;
+            case ErrorPhase::Parser: phaseStr = "Parser";
+                break;
+            case ErrorPhase::Semantic: phaseStr = "Semantic analysis";
+                break;
+            case ErrorPhase::Runtime: phaseStr = "Runtime";
+                break;
+        }
+        std::cerr << "[" << phaseStr << " Error] on line " << token.line << ": " << message << std::endl;
+        if (!tips.empty())
+            std::cerr << tips << std::endl;
+    }
+    errors.clear();
 
     std::exit(1);
 }
--- src/error/Error.hpp
+++ src/error/Error.hpp
@@ -1,18 +1,38 @@
 #pragma once
 #include <string>
+#include <vector>
 
-// Simple code Block for Error Raising.
-namespace Error
-    {
-        // Determine which phase of the interpreter the error has occurred.
-        enum class Phase
-        {
-            Lexer,
-            Parser,
-            Semantic,
-            Runtime
-        };
+#include "../lexer/Token.hpp"
 
-        // Prints out the error and stops execution.
-        [[noreturn]] void raise(Phase phase, const std::string &message, int line, const std::string &tips = "");
-    }
+// Determine which phase of the interpreter the error has occurred.
+enum class ErrorPhase
+{
+    Lexer,
+    Parser,
+    Semantic,
+    Runtime
+};
+
+// Struct to store error data.
+struct ErrorNode
+{
+    ErrorPhase phase;
+    Token token;
+    std::string message;
+    std::string tips;
+};
+
+// Wrapper class for an ErrorNode vector.
+class ErrorList
+{
+public:
+    // Appends the found error at the end of the list.
+    void add(const ErrorPhase &phase, const std::string &message, const Token &token, const std::string &tips = "");
+
+    // Prints out in the terminal all of the current errors in the list.
+    // First-in First-out order.
+    void raise(const ErrorPhase &phase);
+
+private:
+    std::vector<ErrorNode> errors;
+};
